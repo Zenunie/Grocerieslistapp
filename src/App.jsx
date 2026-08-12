e4bd936d3e8d84d04033437f35c569905d920920
Ensar Zenuni <ensar.zenuni@aalto.fi>
2026-08-12 11:48:37 +0300

    Groceries list app

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import AddItem from "./AddItem";
+
+function App() {
+  const [items, setItems] = useState(() => {
+    const datalist = localStorage.getItem("shoppinglist");
+    return datalist ? JSON.parse(datalist) : [];
+  });
+
+  const [newItem, setNewItem] = useState("");
+
+  const addItem = (item) => {
+    const id = crypto.randomUUID();
+    const myNewItem = {
+      id,
+      checked: false,
+      item,
+    };
+
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  };
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item,
+    );
+    setAndSaveItems(listItems);
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
+  };
+  return (
+    <div className="App">
+      <Header title="Groceries" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
+    </div>
+  );
+}
+
+export default App;
